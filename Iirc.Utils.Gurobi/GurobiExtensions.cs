610c6331310c004b6ab23656f46e8bff737a7830
Istvan Modos <istvan.modos@cvut.cz>
2018-12-14 13:31:30 +0100

    Add MIT license

--- Iirc.Utils.Gurobi/GurobiExtensions.cs
+++ Iirc.Utils.Gurobi/GurobiExtensions.cs
@@ -1,3 +1,9 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="GurobiExtensions.cs" company="Czech Technical University in Prague">
+//   Copyright (c) 2018 Czech Technical University in Prague
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
 namespace Iirc.Utils.Gurobi
 {
     using System;
